3a01522fc995604dc323671a7893c17ad97b06ad
nagopi <nagopi@iu.edu>
2023-09-27 16:43:02 -0400

    code improvements

--- mvp/bballrefscraper.py
+++ mvp/bballrefscraper.py
@@ -1,12 +1,12 @@
-# %%
+
 import requests
 import os
 import shutil
 
-# %%
+
 years = list(range(1991,2022))
 
-# %%
+
 url_start = "https://www.basketball-reference.com/awards/awards_{}.html"
 
 for year in years:
@@ -17,35 +17,35 @@ for year in years:
     with open("mvp/{}.html".format(year), "w+") as f:
         f.write(data.text)
 
-# %%
+
 from bs4 import BeautifulSoup
 
-# %%
+
 with open("mvp/1991.html") as f:
     page = f.read()
     
 soup = BeautifulSoup(page, 'html.parser')
 soup.find('tr', class_="over_header").decompose()
 
-# %%
+
 mvp_table = soup.find_all(id="mvp")[0]
 
-# %%
+
 import pandas as pd
 
-# %%
+
 mvp_1991 = pd.read_html(str(mvp_table))[0]
 
-# %%
+
 mvp_1991.head(1)
 
-# %%
+
 mvp_1991["Year"] = 1991
 
-# %%
+
 mvp_1991.head()
 
-# %%
+
 dfs = []
 for year in years:
     with open("mvp/{}.html".format(year)) as f:
@@ -58,15 +58,15 @@ for year in years:
     mvp_df["Year"] = year
     dfs.append(mvp_df)
 
-# %%
+
 mvps = pd.concat(dfs)
 
 mvps.tail()
 
-# %%
+
 mvps.to_csv("mvps.csv")
 
-# %%
+
 player_stats_url = "https://www.basketball-reference.com/leagues/NBA_{}_per_game.html"
 
 for year in years:
@@ -77,19 +77,19 @@ for year in years:
     with open("player/{}.html".format(year), "w+") as f:
         f.write(data.text)
 
-# %%
+
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
 
-# %%
+
 driver = webdriver.Chrome(
     executable_path="/Users/vik/chromedriver"
     )
 
-# %%
+
 for year in years:
     url = player_stats_url.format(year)
     
@@ -100,7 +100,7 @@ for year in years:
     with open("player/{}.html".format(year), "w+") as f:
         f.write(driver.page_source)
 
-# %%
+
 dfs = []
 for year in years:
     with open("player/{}.html".format(year)) as f:
@@ -113,19 +113,19 @@ for year in years:
     player_df["Year"] = year
     dfs.append(player_df)
 
-# %%
+
 players = pd.concat(dfs)
 
-# %%
+
 players.head()
 
-# %%
+
 players.to_csv("players.csv")
 
-# %%
+
 team_stats_url = "https://www.basketball-reference.com/leagues/NBA_{}_standings.html"
 
-# %%
+
 for year in years:
     url = team_stats_url.format(year)
     
@@ -134,7 +134,7 @@ for year in years:
     with open("team/{}.html".format(year), "w+") as f:
         f.write(data.text)
 
-# %%
+
 dfs = []
 for year in years:
     with open("team/{}.html".format(year)) as f:
@@ -156,19 +156,20 @@ for year in years:
     del w_df["Western Conference"]
     dfs.append(w_df)
 
-# %%
+
 teams = pd.concat(dfs)
 
-# %%
+
 teams.tail()
 
-# %%
+
 teams.head()
 
-# %%
+
 teams.to_csv("teams.csv")
 
-# %%
+
+
 
 
 
--- mvp/mlmodel.py
+++ mvp/mlmodel.py
@@ -1,71 +1,71 @@
-# %%
+
 import pandas as pd
 
-# %%
+
 stats = pd.read_csv("player_mvp_stats.csv", index_col=0)
 
-# %%
+
 stats
 
-# %%
+
 pd.isnull(stats).sum()
 
-# %%
+
 stats[pd.isnull(stats["3P%"])][["Player", "3PA"]].head()
 
-# %%
+
 stats[pd.isnull(stats["FT%"])][["Player", "FTA"]].head()
 
-# %%
+
 stats = stats.fillna(0)
 
-# %%
+
 stats.columns
 
-# %%
+
 predictors = ["Age", "G", "GS", "MP", "FG", "FGA", 'FG%', '3P', '3PA', '3P%', '2P', '2PA', '2P%', 'eFG%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', 'W', 'L', 'W/L%',
        'GB', 'PS/G', 'PA/G', 'SRS']
 
-# %%
+
 train = stats[~(stats["Year"] == 2021)]
 test = stats[stats["Year"] == 2021]
 
-# %%
+
 from sklearn.linear_model import Ridge
 
 reg = Ridge(alpha=.1)
 
-# %%
+
 reg.fit(train[predictors],train["Share"])
 
-# %%
+
 predictions = reg.predict(test[predictors])
 predictions = pd.DataFrame(predictions, columns=["predictions"], index=test.index)
 
-# %%
+
 combination = pd.concat([test[["Player", "Share"]], predictions], axis=1)
 
-# %%
+
 combination.sort_values("Share", ascending=False).head(20)
 
-# %%
+
 from sklearn.metrics import mean_squared_error
 
 mean_squared_error(combination["Share"], combination["predictions"])
 
-# %%
+
 combination["Share"].value_counts()
 
-# %%
+
 actual = combination.sort_values("Share", ascending=False)
 predicted = combination.sort_values("predictions", ascending=False)
 actual["Rk"] = list(range(1,actual.shape[0]+1))
 predicted["Predicted_Rk"] = list(range(1,predicted.shape[0]+1))
 
-# %%
+
 actual.merge(predicted, on="Player").head(5)
 
-# %%
+
 def find_ap(combination):
     actual = combination.sort_values("Share", ascending=False).head(5)
     predicted = combination.sort_values("predictions", ascending=False)
@@ -80,16 +80,16 @@ def find_ap(combination):
 
     return sum(ps) / len(ps)
 
-# %%
+
 ap = find_ap(combination)
 
-# %%
+
 ap
 
-# %%
+
 years = list(range(1991,2022))
 
-# %%
+
 aps = []
 all_predictions = []
 for year in years[5:]:
@@ -102,10 +102,10 @@ for year in years[5:]:
     all_predictions.append(combination)
     aps.append(find_ap(combination))
 
-# %%
+
 sum(aps) / len(aps)
 
-# %%
+
 def add_ranks(predictions):
     predictions = predictions.sort_values("predictions", ascending=False)
     predictions["Predicted_Rk"] = list(range(1,predictions.shape[0]+1))
@@ -114,10 +114,10 @@ def add_ranks(predictions):
     predictions["Diff"] = (predictions["Rk"] - predictions["Predicted_Rk"])
     return predictions
 
-# %%
+
 add_ranks(all_predictions[1])
 
-# %%
+
 def backtest(stats, model, years, predictors):
     aps = []
     all_predictions = []
@@ -133,59 +133,59 @@ def backtest(stats, model, years, predictors):
         aps.append(find_ap(combination))
     return sum(aps) / len(aps), aps, pd.concat(all_predictions)
 
-# %%
+
 mean_ap, aps, all_predictions = backtest(stats, reg, years[5:], predictors)
 
-# %%
+
 mean_ap
 
-# %%
+
 all_predictions[all_predictions["Rk"] < 5].sort_values("Diff").head(10)
 
-# %%
+
 pd.concat([pd.Series(reg.coef_), pd.Series(predictors)], axis=1).sort_values(0, ascending=False)
 
-# %%
+
 stat_ratios = stats[["PTS", "AST", "STL", "BLK", "3P", "Year"]].groupby("Year").apply(lambda x: x/x.mean())
 
-# %%
+
 stats[["PTS_R", "AST_R", "STL_R", "BLK_R", "3P_R"]] = stat_ratios[["PTS", "AST", "STL", "BLK", "3P"]]
 
-# %%
+
 predictors += ["PTS_R", "AST_R", "STL_R", "BLK_R", "3P_R"]
 
-# %%
+
 mean_ap, aps, all_predictions = backtest(stats, reg, years[5:], predictors)
 
-# %%
+
 mean_ap
 
-# %%
+
 stats["NPos"] = stats["Pos"].astype("category").cat.codes
 stats["NTm"] = stats["Tm"].astype("category").cat.codes
 
-# %%
+
 from sklearn.ensemble import RandomForestRegressor
 
 rf = RandomForestRegressor(n_estimators=50, random_state=1, min_samples_split=5)
 
 mean_ap, aps, all_predictions = backtest(stats, rf, years[28:], predictors + ["NPos", "NTm"])
 
-# %%
+
 mean_ap
 
-# %%
+
 mean_ap, aps, all_predictions = backtest(stats, reg, years[28:], predictors)
 
-# %%
+
 mean_ap
 
-# %%
+
 from sklearn.preprocessing import StandardScaler
 
 sc = StandardScaler()
 
-# %%
+
 def backtest(stats, model, years, predictors):
     aps = []
     all_predictions = []
@@ -204,16 +204,16 @@ def backtest(stats, model, years, predictors):
         aps.append(find_ap(combination))
     return sum(aps) / len(aps), aps, pd.concat(all_predictions)
 
-# %%
+
 mean_ap, aps, all_predictions = backtest(stats, reg, years[28:], predictors)
 
-# %%
+
 mean_ap
 
-# %%
+
 sc.transform(stats[predictors])
 
-# %%
+
 
 
 
